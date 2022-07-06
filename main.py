4d59bcd3416a6181ceb5b4495318d7dae51a1825
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-07-06 13:46:45 +0430

    Data renamed :)))

--- main.py
+++ main.py
@@ -1,54 +1,42 @@
+from sklearn.utils import shuffle
+import tensorflow as tf
+import matplotlib.pyplot as plt
 import numpy as np
-import cv2
-from tqdm import tqdm
 
-class Image:
-    def __init__(self, image_px, bounding_boxes):
-        self.image_px = image_px
-        self.bounding_boxes = bounding_boxes
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+img_height, img_width = 255, 255
+batch_size = 75
+data_path = "./Data/train"
 
-class BoundingBox:
-    def __init__(self, x, y, w, h, landmarks):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.landmarks = landmarks
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_path,
+    validation_split=0.2,
+    shuffle=False,
+    subset="training",
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
-
-def read_data():
-    data_desc = open("./Data/annotations.txt", 'r')
-    data_path = "./Data/train/"
-    lines = data_desc.readlines()
-    pointer = 0
-    images_list = []
-    pbar = tqdm(total=4275, initial=0)
-
-    while pointer < len(lines):
-        path = data_path + lines[pointer][:-1]
-        image_px = cv2.imread(path)
-        pointer += 1
-        n = int(lines[pointer])
-        pointer += 1
-        bounding_boxes = []
-        for i in range(n):
-            temp = lines[pointer]
-            temp = temp.split()
-            landmarks = temp[4:]
-            bounding_box = BoundingBox(temp[0], temp[1], temp[2], temp[3], landmarks)
-            bounding_boxes.append(bounding_box)
-            pointer += 1
-        image = Image(image_px, bounding_boxes)
-        images_list.append(image)
-        pbar.update(1)
-        
-    pbar.close()
-
-    return images_list
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_path,
+    validation_split=0.2,
+    subset="validation",
+    shuffle=False,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 
-images_list = read_data()
-# model = ?
 
 
+# model = tf.keras.Sequential([
+#   tf.keras.layers.Rescaling(1./255),
+#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#   tf.keras.layers.MaxPooling2D(),
+#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#   tf.keras.layers.MaxPooling2D(),
+#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#   tf.keras.layers.MaxPooling2D(),
+#   tf.keras.layers.Flatten(),
+#   tf.keras.layers.Dense(128, activation='relu'),
+#   tf.keras.layers.Dense(num_classes)
+# ])
