19ccb82fd9087a2228dfe15f99acfb8e43753b4a
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-07-06 16:21:29 +0430

    newer data

--- main.py
+++ main.py
@@ -1,42 +1,24 @@
-from sklearn.utils import shuffle
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-img_height, img_width = 255, 255
+cell_count = 8
+img_height, img_width = 256, 256
 batch_size = 75
 data_path = "./Data/train"
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
+full_ds = tf.keras.utils.image_dataset_from_directory(
     data_path,
-    validation_split=0.2,
-    shuffle=False,
-    subset="training",
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
-
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_path,
-    validation_split=0.2,
-    subset="validation",
     shuffle=False,
     image_size=(img_height, img_width),
     batch_size=batch_size)
 
+# Add Labels
+
+# Training, Testing
+
+# Model
+
+# Non Maximum Suppression
 
 
-
-# model = tf.keras.Sequential([
-#   tf.keras.layers.Rescaling(1./255),
-#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
-#   tf.keras.layers.MaxPooling2D(),
-#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
-#   tf.keras.layers.MaxPooling2D(),
-#   tf.keras.layers.Conv2D(32, 3, activation='relu'),
-#   tf.keras.layers.MaxPooling2D(),
-#   tf.keras.layers.Flatten(),
-#   tf.keras.layers.Dense(128, activation='relu'),
-#   tf.keras.layers.Dense(num_classes)
-# ])
